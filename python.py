71603e1dfecb902b50f5483c24c1232100503938
vans-aj <vnshajrawat951@gmail.com>
2025-02-24 01:31:54 +0530

    added dependencies

---
+++ python.py
@@ -0,0 +1,8 @@
+#imporing the dependencies
+import pandas as pd;
+import numpy as np;
+import matplotlib.pyplot as plt;
+import seaborn as sns;
+from sklearn.model_selection import train_test_split;
+from xgboost import XGBRegressor;
+from sklearn import metrics;
